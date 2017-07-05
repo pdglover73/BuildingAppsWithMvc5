4e6fc3bf0c2528afe498a838b096986a0d171d0e
pdglover <paul.glover@riverside.org.uk>
2017-07-05 11:23:30 +0100

    Initial commit of base project

---
+++ BuildingAppsWithMvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuildingAppsWithMvc5.MvcApplication" Language="C#" %>
---
+++ BuildingAppsWithMvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuildingAppsWithMvc5.Startup))]
+namespace BuildingAppsWithMvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
